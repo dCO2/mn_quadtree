14a7101212e710d1686d43a338048a019c2b7ede
Caleb Durojaiye <dco2.caleb@gmail.com>
2023-09-25 17:55:06 +0100

    final p5 boilerplate

--- main.js
+++ main.js
@@ -1,27 +1,44 @@
 new p5(
   (p) => {
-  p.setup = () => {
-    const canvasOne = p.createCanvas(400, 400);
-    canvasOne.parent("canvas-1");
-  }
+    
+    var ballx = Math.floor(Math.random() * 400) + 1;
+    var bally = Math.floor(Math.random() * 400) + 1;
+    var dx = 4 * Math.random() - 2;
+    var dy = 4 * Math.random() - 2;
 
-  p.draw = () => {
-    p.clear();
-    p.background("#b7410e");
-    p.ellipse(p.mouseX, p.mouseY, 20, 20);
-  }
+    p.setup = () => {
+      const canvasOne = p.createCanvas(400, 400);
+      canvasOne.parent("canvas-1");
+    }
+
+    p.draw = () => {
+      p.clear();
+      p.background("#b7410e");
+      p.ellipse(ballx, bally, 20, 20);
+      bally += dy;
+      ballx += dx;
+
+      if(bally >= 400 || bally <= 0){
+        dy *= -1;
+      }
+      
+      if(ballx >= 400 || ballx <= 0){
+        dx *= -1;
+      }
+
+    }
 });
 
 new p5(
   (p) => {
-  p.setup = () => {
-    const canvasTwo = p.createCanvas(400, 400);
-    canvasTwo.parent("canvas-2");
-  }
+    p.setup = () => {
+      const canvasTwo = p.createCanvas(400, 400);
+      p.background("#e7f85e");
+      canvasTwo.parent("canvas-2");
+    }
 
-  p.draw = () => {
-    p.clear();
-    p.background("#e7f85e");
-    p.ellipse(p.mouseX, p.mouseY, 20, 20);
-  }
+    p.draw = () => {
+      p.noStroke();
+      p.ellipse(p.mouseX, p.mouseY, 20, 20);
+    }
 });
